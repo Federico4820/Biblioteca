8f5ca6783719619e0acb3b8522d87a0e2b3b9a9c
Federico4820 <federicolepore1@gmail.com>
2025-03-10 17:01:28 +0100

    Aggiungere i file di progetto.

---
+++ Biblioteca/Data/BibliotecaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Data
+{
+    public class BibliotecaDbContext : DbContext
+    {
+        public BibliotecaDbContext(DbContextOptions<BibliotecaDbContext> options ) : base(options)
+        {
+        }
+
+        public DbSet<Book> books { get; set; }
+
+        protected BibliotecaDbContext()
+        {
+        }
+    }
+}
